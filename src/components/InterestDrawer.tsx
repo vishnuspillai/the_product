7d4878390b4f487d48dc0b4bafe41e8548a54998
Vishnu S Pillai <143305459+vishnuspillai@users.noreply.github.com>
2025-06-19 15:36:59 +0530

    Updated InterestDrawer.tsx

--- src/components/InterestDrawer.tsx
+++ src/components/InterestDrawer.tsx
@@ -34,8 +34,8 @@ const InterestDrawer: React.FC<DrawerProps> = ({ isOpen, onClose, type }) => {
             }
           ],
           gallery: [
-            "<iframe src="https://assets.pinterest.com/ext/embed.html?id=1103733821198468410" height="713" width="345" frameborder="0" scrolling="no" > </iframe> 
-        
+            "<iframe src="https://assets.pinterest.com/ext/embed.html?id=1103733821198468410" height="713" width="345" frameborder="0" scrolling="no" ></iframe>
+?auto=compress&cs=tinysrgb&w=400",
             "https://images.pexels.com/photos/1252869/pexels-photo-1252869.jpeg?auto=compress&cs=tinysrgb&w=400",
             "https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&w=400",
             "https://images.pexels.com/photos/1024993/pexels-photo-1024993.jpeg?auto=compress&cs=tinysrgb&w=400"

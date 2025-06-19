5a643e2126b23a29c8b64e18f78a67aace59ad88
Vishnu S Pillai <143305459+vishnuspillai@users.noreply.github.com>
2025-06-19 20:41:37 +0530

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -52,7 +52,7 @@ const Hero = () => {
 
           {/* Scroll Indicator */}
           <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-            <ArrowDown className="text-white/60" size={24} />
+            <ArrowDown className="text-white/60" size={16} />
           </div>
         </div>
       </div>

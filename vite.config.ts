d88e5b13ff216be26fa55e6fa060a54dd7e6c15b
Vishnu S Pillai <143305459+vishnuspillai@users.noreply.github.com>
2025-07-12 21:31:44 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/the_product/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
